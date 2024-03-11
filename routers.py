45a5d9462e367198f2b562bc983bc7d2b0340ce9
AyushUnleashed <myzealouslifeofficial@gmail.com>
2024-03-12 03:19:40 +0530

    adding base router

--- routers.py
+++ routers.py
@@ -2,4 +2,6 @@ from fastapi import APIRouter
 # webhook_router = APIRouter()
 #
 basic_router = APIRouter()
-
+@basic_router.get("/")
+async def read_root():
+    return {"message": "Welcome to the superhero ai backend gateway. This is the root endpoint."}
